04642a599a436e3bf1b4129e65b329dd8371f666
ulimx <ulimx@RCK23>
2023-02-05 15:25:09 -0600

    Agregar archivos de proyecto.

---
+++ Controllers/PortafolioController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Portafolio.Models;
+using System.Diagnostics;
+
+namespace Portafolio.Controllers
+{
+    public class PortafolioController : Controller
+    {
+        private readonly ILogger<PortafolioController> _logger;
+
+        public PortafolioController(ILogger<PortafolioController> logger)
+        {
+            _logger = logger;
+            
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SobreMi()
+        {
+            return View();
+        }
+
+        public IActionResult Proyectos()
+        {
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
