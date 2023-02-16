9831c4199a72db99fbbc302e2016d39a9c768bbf
Ulises Martinez <ulimxtz@gmail.com>
2023-02-16 14:12:40 -0600

    Arreglo de Vista proyectos

--- Program.cs
+++ Program.cs
@@ -11,38 +11,32 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews()
     .AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix)
     .AddDataAnnotationsLocalization(opciones =>
-    {   //TRADUCIR LAS ANOTACIONES DE DATOS
+    {   
         opciones.DataAnnotationLocalizerProvider = (_, factoria) =>
             factoria.Create(typeof(RecursoCompartido));
     });
 
 builder.Services.AddTransient<IRepositorioProyectos, RepositorioProyectos>();
 
-//LOCALIZACIÓN EN EL PROYECTO (para utilizar varios idiomas)
 builder.Services.AddLocalization(opciones =>
 {
-    //Agregacion de los recursos (en este caso idioma diferente)
     opciones.ResourcesPath = "Resources";
 });
 
 var app = builder.Build();
 
-//AGREGAR DISTINTOS IDIOMAS AL PROYECTO
-//LOCALIZAR LAS PETICIONES DEL USUARIO
+
 app.UseRequestLocalization(opciones =>
 {
-    // PONER IDIOMA POR DEFAULT (indicar)
     opciones.DefaultRequestCulture = new RequestCulture("es");
 
     opciones.SupportedUICultures = Constantes.CulturasUISoportadas
         .Select(cultura => new CultureInfo(cultura.Value)).ToList();
 });
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
